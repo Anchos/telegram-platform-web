b52e44a13f826382d020fe7b672a606f8c5bc6f9
Kirill Lebedev <k.lebedev@slemma.com>
2018-07-25 19:29:52 +0500

    add redux logger middleware

--- src/store/store.js
+++ src/store/store.js
@@ -1,16 +1,23 @@
-import { createStore, combineReducers, applyMiddleware } from "redux";
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import createSagaMiddleware from "redux-saga";
+import reduxLogger from 'redux-logger';
 import { all } from "redux-saga/effects";
 import * as sagas from "./sagas";
 import * as reducers from "./reducers";
 
 const saga = createSagaMiddleware();
+const middlewares = [saga];
+
+if(process.env.NODE_ENV === 'development') {
+  middlewares.push(reduxLogger);
+}
+
 const store = createStore(
   combineReducers({
     connection: reducers.connection,
     main: reducers.mainPage,
   }),
-  applyMiddleware(saga),
+  applyMiddleware(...middlewares),
 );
 
 saga.run(function*() {
@@ -19,5 +26,4 @@ saga.run(function*() {
     data: sagas.data(),
   });
 });
-
 export default store;
