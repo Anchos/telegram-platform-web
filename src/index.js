ad028cba39847111900358d186251fa1b2a71ad2
Kirill Lebedev <k.lebedev@slemma.com>
2018-09-10 19:03:07 +0500

    remove legacy components, remove bootstrap, remove styled components, fix package.json (move devDependencies, remove unused packages)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import * as React from "react";
 import { render } from "react-dom";
 import { Provider } from "react-redux";
-import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./components/App";
 import store from "./store/store";
 import './style.css';
--- src/ui/newdesign/select/styles.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-
-export const Wrapper = styled.div`
-  width: 50px;
-`
-
-export const StyledSelect = styled.select`
-  width: 100%;
-`
